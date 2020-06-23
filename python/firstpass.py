c4f52867297e11069657231d814deb0fd375cc86
Sam Chen <anachronicnomad@gmail.com>
2020-06-22 23:07:03 -0600

    first pass done?

---
+++ python/firstpass.py
@@ -0,0 +1,58 @@
+#!python3
+
+import pandas as pd
+import urllib
+import xmltodict
+import argparse
+
+def AddDescriptions(input_excel="", output_excel="", sheet_name=""):
+    # read the input excel file
+    xl_file = pd.ExcelFile(input_excel)
+    # make a Python dictionary, each sheet is key'd by name foreach sheet in excel file
+    wkbk = {sheet_name: xl_file.parse(sheet_name) for sheet_name in xl_file.sheet_names}
+
+    nrows = len(wkbk[sheet_name].index)
+
+    wkbk[sheet_name]['HMDB_Description'] = pd.Series([""]*nrows,
+                                                        index=wkbk[sheet_name].index)
+    
+    is_na_value = wkbk[sheet_name].isnull()
+
+    for row_idx in range(0, nrows):
+        if is_na_value.loc[row_idx, 'HMP ID'] == False:
+            hmdb_id = wkbk[sheet_name].loc[row_idx, 'HMP ID']
+            hmdb_id = "HMDB00" + hmdb_id[4::]
+            print("Doing ", hmdb_id)
+            url_str = "https://hmdb.ca/metabolites/"+hmdb_id+".xml"
+            try:
+                with urllib.request.urlopen(url_str) as response:
+                    hmdb_xml = response.read()
+                    xml_data = xmltodict.parse(hmdb_xml)
+
+                    desc_str = xml_data['metabolite']['description']
+                    wkbk[sheet_name].loc[row_idx, 'HMDB_Description'] = desc_str 
+            except:
+                print(hmdb_id, " FAILED")
+                continue
+
+    with pd.ExcelWriter(output_excel) as writer:
+        for sheet in [sn for sn in xl_file.sheet_names]:
+            wkbk[sheet].to_excel(writer, sheet_name=sheet, engine='xlsxwriter')
+
+
+if __name__ == "__main__":
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument("input_excel", 
+                           help="filepath to input excel workbook",
+                           type=str)
+    argparser.add_argument("output_excel",
+                           help="filepath to write output excel workbook",
+                           type=str)
+    argparser.add_argument("sheet_name",
+                           help="the name of the excel sheet",
+                           type=str)
+    args = argparser.parse_args()
+    #print(args.input_excel)
+    AddDescriptions(input_excel=args.input_excel,
+                    output_excel=args.output_excel,
+                    sheet_name=args.sheet_name)
